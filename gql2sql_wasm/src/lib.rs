2237950e85bbf0c323788a710395b42d4cb40e8c
Nick Randall <nicksrandall@gmail.com>
2023-01-02 14:28:55 -0700

    fixing wasm build

--- gql2sql_wasm/src/lib.rs
+++ gql2sql_wasm/src/lib.rs
@@ -2,28 +2,18 @@ use gql2sql::gql2sql as gql2sql_rs;
 use graphql_parser::query::parse_query;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn gql2sql(query: &str) -> JsValue {
     let gqlast = parse_query::<&str>(query).unwrap();
-    let statements = gql2sql_rs(gqlast).unwrap();
-    let queries = statements
-        .iter()
-        .map(std::string::ToString::to_string)
-        .collect::<Vec<_>>();
-
-    serde_wasm_bindgen::to_value(&queries).unwrap()
+    let query = gql2sql_rs(gqlast).unwrap().to_string();
+    serde_wasm_bindgen::to_value(&query).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-
-
     #[test]
     fn it_works() {
         assert_eq!(true, true);
