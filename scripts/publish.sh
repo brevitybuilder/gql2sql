545acbb65db73a167b068fe002eb516d07df3bbf
Nick Randall <nicksrandall@gmail.com>
2023-01-10 12:22:00 -0700

    adding publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+git update-index -q --refresh
+if ! git diff-index --quiet HEAD --; then
+  echo "Working directory not clean, please commit your changes first"
+  exit
+fi
+
+bump=$1
+
+cd gql2sql_node
+npm --no-git-tag-version version "$bump" > ../version.txt
+cd ..
+
+git add .
+git commit -m "$(< version.txt)"
+git push
+
+rm version.txt
