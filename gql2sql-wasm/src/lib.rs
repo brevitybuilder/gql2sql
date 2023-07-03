3482bf06d6f8ef930f8cd788e3836faee703796c
Nick Randall <nicksrandall@gmail.com>
2023-07-03 13:09:09 -0600

    bump

--- gql2sql-wasm/src/lib.rs
+++ gql2sql-wasm/src/lib.rs
@@ -14,7 +14,8 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[derive(Deserialize)]
 pub struct Args {
     pub query: String,
-    pub vars: Option<Value>,
+    pub variables: Option<Value>,
+    pub operation_name: Option<String>,
 }
 
 #[derive(Serialize)]
@@ -27,9 +28,9 @@ pub struct GqlResult {
 #[wasm_bindgen]
 pub fn gql2sql(mut args: String) -> Result<String, JsError> {
     set_panic_hook();
-    let Args { query, vars } = unsafe { simd_json::from_str(&mut args)? };
+    let Args { query, variables, operation_name } = unsafe { simd_json::from_str(&mut args)? };
     let ast = parse_query(query)?;
-    let (sql, params, tags) = gql2sql_rs(ast, &vars, None).map_err(|e| JsError::new(&e.to_string()))?;
+    let (sql, params, tags) = gql2sql_rs(ast, &variables, operation_name).map_err(|e| JsError::new(&e.to_string()))?;
     let result = GqlResult {
         sql: sql.to_string(),
         params,
