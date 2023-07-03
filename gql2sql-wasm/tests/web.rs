00788da8f7ef23c8708e7bbd988a65898ec1608f
Nick Randall <nicksrandall@gmail.com>
2023-07-03 10:56:13 -0600

    adding wasm support

---
+++ gql2sql-wasm/tests/web.rs
@@ -0,0 +1,15 @@
+//! Test suite for the Web and headless browsers.
+
+#![cfg(target_arch = "wasm32")]
+
+extern crate wasm_bindgen_test;
+use wasm_bindgen::JsError;
+use wasm_bindgen_test::*;
+use gql2sql_wasm::gql2sql;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn pass() {
+    assert_eq!(1 + 1, 2);
+}
